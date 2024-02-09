16136982c3d50a3c1389c25491d37cb9de9bc652
Idriss Douiri <drisspennywise@gmail.com>
2024-02-09 18:07:40 +0100

    optimizing v2

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,28 +1,28 @@
 import {
-  ReactOriginal,
-  TailwindcssPlain,
-  JavascriptOriginal,
-  Html5Original,
-  Css3Original,
-  NextjsOriginal,
-  TypescriptOriginal,
-  GithubOriginal,
-  GitOriginal,
-  MysqlOriginal,
-} from "devicons-react";
+  css,
+  html,
+  js,
+  nextjs,
+  reactjs,
+  tailwindcss,
+  ts,
+  github,
+  git,
+  mysql,
+} from "../assets/icons";
 
 const Skills = () => {
   const skills = [
-    { Tag: ReactOriginal, label: "Reactjs" },
-    { Tag: TailwindcssPlain, label: "TailwindCSS" },
-    { Tag: JavascriptOriginal, label: "JavaScript" },
-    { Tag: Html5Original, label: "HTML" },
-    { Tag: Css3Original, label: "CSS" },
-    { Tag: NextjsOriginal, label: "Nextjs" },
-    { Tag: TypescriptOriginal, label: "TypeScript" },
-    { Tag: GithubOriginal, label: "Github" },
-    { Tag: GitOriginal, label: "Git" },
-    { Tag: MysqlOriginal, label: "MySQL" },
+    { img: reactjs, label: "Reactjs" },
+    { img: tailwindcss, label: "TailwindCSS" },
+    { img: js, label: "JavaScript" },
+    { img: html, label: "HTML" },
+    { img: css, label: "CSS" },
+    { img: nextjs, label: "Nextjs" },
+    { img: ts, label: "TypeScript" },
+    { img: github, label: "Github" },
+    { img: git, label: "Git" },
+    { img: mysql, label: "MySQL" },
   ];
 
   return (
@@ -31,14 +31,14 @@ const Skills = () => {
         Skills
       </h2>
       <ul className="flex flex-wrap gap-4 justify-center">
-        {skills.map(({ Tag, label }, i) => (
+        {skills.map(({ img, label }, i) => (
           <li
             key={i}
             className="w-20 h-20 rounded-full bg-rose-50 flex justify-center items-center before:border before:border-gray-400 before:absolute relative before:inset-0 before:rounded-full before:-left-1 before:-top-1"
             title={label}
           >
             <span className="sr-only">{label}</span>
-            <Tag size={30} />
+            <img src={img.src} alt="" width={30} />
           </li>
         ))}
       </ul>
